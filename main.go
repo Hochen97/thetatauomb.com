db9d7a92a6c830c031e0cd6d334d01cebdf72e60
Jacob Hochendoner <jacob@jrhtec.com>
2018-09-21 21:29:06 -0400

    added og:image tag to index.html

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
@@ -14,15 +12,23 @@ func main() {
 	// Serve frontend static files
 	router.Use(static.Serve("/", static.LocalFile("./views", true)))
 
+	// router.LoadHTMLGlob("./views/*")
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "index.html", nil)
+	// })
+	// router.GET("/events", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "events.html", nil)
+	// })
+
 	// Setup route group for the API
-	api := router.Group("/api")
-	{
-		api.GET("/", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "pong",
-			})
-		})
-	}
+	// api := router.Group("/api")
+	// {
+	// 	api.GET("/", func(c *gin.Context) {
+	// 		c.JSON(http.StatusOK, gin.H{
+	// 			"message": "pong",
+	// 		})
+	// 	})
+	// }
 
 	// Start and run the server
 	router.Run(":3001")
