0ce5a5298a62740cc64cd177251bb7d25ce6318b
Jacob Hochendoner <jacob@jrhtec.com>
2018-10-01 20:20:04 -0400

    fixed alumni route.

--- routes/index.js
+++ routes/index.js
@@ -10,26 +10,32 @@ router.get('/', function(req, res, next) {
 router.get('/brothers', function(req, res, next) {
   res.render('brothers', { title: 'Brothers | Omega Beta — Theta Tau' });
 });
+
 /* GET Actives Page. 
 router.get('/active', function(req, res, next) {
   res.render('brothers', { title: 'Home | Omega Beta — Theta Tau' });
 });
-/* GET Alumni Page. 
+
+/* GET Alumni Page. */
 router.get('/alumni', function(req, res, next) {
   res.render('brothers', { title: 'Home | Omega Beta — Theta Tau' });
 });
+
 /* GET Blog Page. */
 router.get('/blog', function(req, res, next) {
   res.render('blog', { title: 'Blog | Omega Beta — Theta Tau' });
 });
+
 /* GET Events Page. */
 router.get('/events', function(req, res, next) {
   res.render('events', { title: 'Events | Omega Beta — Theta Tau' });
 });
+
 /* GET About Page. */
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About Us | Omega Beta — Theta Tau' });
 });
+
 /* GET Contact Page. */
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact Us | Omega Beta — Theta Tau' });
