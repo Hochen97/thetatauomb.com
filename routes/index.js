da4a8bcf1784c08209db257efa79d584f169d765
Jacob Hochendoner <jacob@jrhtec.com>
2018-10-01 20:00:53 -0400

    I did a whole lot and changed everything over to node.js and ejs

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,33 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET Brothers Page */
+router.get('/brothers', function(req, res, next) {
+  res.render('brothers', { title: 'Express' });
+});
+/* GET Actives Page. 
+router.get('/active', function(req, res, next) {
+  res.render('brothers', { title: 'Express' });
+});
+/* GET Alumni Page. 
+router.get('/alumni', function(req, res, next) {
+  res.render('brothers', { title: 'Express' });
+});
+/* GET Blog Page. */
+router.get('/blog', function(req, res, next) {
+  res.render('blog', { title: 'Express' });
+});
+/* GET Events Page. */
+router.get('/events', function(req, res, next) {
+  res.render('events', { title: 'Express' });
+});
+/* GET About Page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+/* GET Contact Page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+
 module.exports = router;
